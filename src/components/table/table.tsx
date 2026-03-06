d87de91f01bb94b43fe209bb53922de2b3562519
Marc Blais <blaisma@live.fr>
2026-03-05 20:03:10 -0500

    Update table component to use min-w-0 for better responsiveness in overflow handling

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -8,7 +8,7 @@ export type TableProps<T> = {
 export default function Table<T>({ children, items = [] }: TableProps<T>) {
   return (
     <div className="flex w-full h-full min-h-0 overflow-hidden rounded-lg border border-stone-200">
-      <div className="flex flex-col gap-0.5 w-full min-h-0 flex-1 overflow-y-auto overflow-x-hidden">
+      <div className="flex flex-col gap-0.5 w-full min-w-0 min-h-0 flex-1 overflow-y-auto overflow-x-hidden">
         {items.map((item) => children(item))}
       </div>
     </div>
